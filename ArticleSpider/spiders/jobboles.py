6affc82ae6ad21e7781f895f5a2a2c4890bd119b
acutesun <sunsec@yeah.net>
2017-05-09 20:49:19 +0800

    process zhihu question item and answer item

--- ArticleSpider/spiders/jobboles.py
+++ ArticleSpider/spiders/jobboles.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
-import datetime
 from scrapy.http import Request
 from urllib import parse
-from ArticleSpider.items import AticleItem
+from ArticleSpider.items import AticleItem, AticleItemLoader
 from ArticleSpider.utils import common
+from scrapy.loader import ItemLoader
 
 
 class JobbolesSpider(scrapy.Spider):
@@ -14,78 +14,85 @@ class JobbolesSpider(scrapy.Spider):
     start_urls = ["http://blog.jobbole.com/all-posts"]
 
     def parse(self, response):
-        # 1. 获取当前页面的 article url 和image_url列表, 并交给scrapy下载
-        post_nodes = response.css('#archive .floated-thumb .post-thumb a')
+
+        post_nodes = response.css('#archive .floated-thumb .post-thumb a')  # 1.获取当前页面的url图片url列表并交给scrapy下载
         for post_node in post_nodes:
             post_url = post_node.css('::attr(href)').extract_first()
-            # 获取图片的url
-            image_url = post_node.css('img::attr(src)').extract_first()
-            print('image_url', image_url)
-            print(post_url)
+            image_url = post_node.css('img::attr(src)').extract_first()     # 获取图片的url
             yield Request(url=parse.urljoin(response.url, post_url),        # 注意这里我暂时改为post_url，因为数据太多了。
-                          meta={'front_image_url': image_url}, callback=self.parse_css)
+                          meta={'front_image_url': image_url}, callback=self.parse_detail)
 
-        # 2. 获取下一页url列表并交给scrapy下载
-        # <a class ="next page-numbers" href="http://blog.jobbole.com/all-posts/page/3/" > 下一页 » < / a >
-        # 两个平级的class中间不使用空格
-        next_url = response.css('.next.page-numbers::attr(href)').extract_first()
-        print(next_url)
+        next_url = response.css('.next.page-numbers::attr(href)').extract_first()  # 2. 获取下一页url列表并交给scrapy下载
         if next_url:
             print(parse.urljoin(response.url, next_url))
             yield Request(parse.urljoin(response.url, post_url), callback=self.parse)
             print('yield request')
 
-    def parse_css(self, response):
-        article_item = AticleItem()
+    def parse_detail(self, response):
 
         front_image_url = response.meta.get('front_image_url', '')  # 获取文章封面图
-        print('front_image_url', front_image_url)
+        loader = AticleItemLoader(item=AticleItem(), response=response)
+        loader.add_value('url_object_id', common.get_md5(response.url))
+        loader.add_value('front_image_url', front_image_url)
+        loader.add_value('url', response.url)
+        loader.add_css('title', '.entry-header h1::text')
+        loader.add_css('create_time', 'p.entry-meta-hide-on-mobile::text')
+        loader.add_css('great_nums', '.post-adds .vote-post-up h10::text')
+        loader.add_css('bookmark_nums', '.post-adds .bookmark-btn::text')
+        loader.add_css('comment_nums', 'a[href="#article-comment"] span::text')
+        loader.add_css('tags', 'p.entry-meta-hide-on-mobile a::text')
+        loader.add_css('content', 'div.entry')
+        item = loader.load_item()
+        yield item
 
-        # css选择器提取信息 response.css().extract_first() 选取第一个
-        title = response.css('.entry-header h1::text').extract_first()  # 选取所有包含entry-header的class元素下的h1元素的文本内容
-        create_time = response.css('p.entry-meta-hide-on-mobile::text').extract_first()
-        if create_time:
-            create_time = create_time.strip().replace('·', '').strip()
-        # 点赞
-        great_nums = response.css('.post-adds .vote-post-up h10::text').extract_first('')
-        if great_nums:
-            great_nums = int(great_nums)
-        else:
-            great_nums = 0
-        # 收藏
-        bookmark_nums = response.css('.post-adds .bookmark-btn::text').extract_first('')
-        bookmark_re = re.match(r'.*?(\d+).*', bookmark_nums)
-        if bookmark_re:
-            bookmark_nums = int(bookmark_re.group(1))
-        else:
-            bookmark_nums = 0
-        # 评论
-        comment_nums = response.css('a[href="#article-comment"] span::text').extract_first('')
-        article_re = re.match(r'.*?(\d+).*', comment_nums)
-        if article_re:
-            comment_nums = int(article_re.group(1))
-        else:
-            comment_nums = 0
-        # 标签内容
-        tag_list = response.css('p.entry-meta-hide-on-mobile a::text').extract()
-        tag_list = [e for e in tag_list if not e.strip().endswith('评论')]
-        tags = ','.join(tag_list)
-        # 所有内容
-        content = response.css('div.entry').extract_first()[:100]
-
-        # 将所有的值填充到item
-        article_item['url_object_id'] = common.get_md5(response.url)
-        article_item['title'] = title
-        article_item['url'] = response.url
-        article_item['front_image_url'] = [front_image_url]
-        article_item['create_time'] = create_time
-        article_item['great_nums'] = great_nums
-        article_item['comment_nums'] = comment_nums
-        article_item['bookmark_nums'] = bookmark_nums
-        article_item['tags'] = tags
-        article_item['content'] = content
-
-        yield article_item  # 生成的item会传入到pipelines中
+    # def parse_css(self, response):
+    #     article_item = AticleItem()
+    #     front_image_url = response.meta.get('front_image_url', '')  # 获取文章封面图
+    #
+    #     # css选择器提取信息 response.css().extract_first() 选取第一个
+    #     title = response.css('.entry-header h1::text').extract_first()  # 选取所有包含entry-header的class元素下的h1元素的文本内容
+    #     create_time = response.css('p.entry-meta-hide-on-mobile::text').extract_first()
+    #     if create_time:
+    #         create_time = create_time.strip().replace('·', '').strip()
+    #
+    #     great_nums = response.css('.post-adds .vote-post-up h10::text').extract_first('')   # 点赞
+    #     if great_nums:
+    #         great_nums = int(great_nums)
+    #     else:
+    #         great_nums = 0
+    #
+    #     bookmark_nums = response.css('.post-adds .bookmark-btn::text').extract_first('')    # 收藏
+    #     bookmark_re = re.match(r'.*?(\d+).*', bookmark_nums)
+    #     if bookmark_re:
+    #         bookmark_nums = int(bookmark_re.group(1))
+    #     else:
+    #         bookmark_nums = 0
+    #
+    #     comment_nums = response.css('a[href="#article-comment"] span::text').extract_first('')  # 评论
+    #     article_re = re.match(r'.*?(\d+).*', comment_nums)
+    #     if article_re:
+    #         comment_nums = int(article_re.group(1))
+    #     else:
+    #         comment_nums = 0
+    #
+    #     tag_list = response.css('p.entry-meta-hide-on-mobile a::text').extract()    # 标签内容
+    #     tag_list = [e for e in tag_list if not e.strip().endswith('评论')]
+    #     tags = ','.join(tag_list)
+    #
+    #     content = response.css('div.entry').extract_first()[:100]   # 所有内容
+    #
+    #     article_item['url_object_id'] = common.get_md5(response.url)
+    #     article_item['title'] = title
+    #     article_item['url'] = response.url
+    #     article_item['front_image_url'] = [front_image_url]
+    #     article_item['create_time'] = create_time
+    #     article_item['great_nums'] = great_nums
+    #     article_item['comment_nums'] = comment_nums
+    #     article_item['bookmark_nums'] = bookmark_nums
+    #     article_item['tags'] = tags
+    #     article_item['content'] = content
+    #
+    #     yield article_item  # 生成的item会传入到pipelines中
 
 
     # def parse_xpath(self, response):
--- ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/spiders/zhihu.py
@@ -4,6 +4,10 @@ from collections import namedtuple
 import re
 import time
 import json
+from urllib import parse
+from scrapy.loader import ItemLoader
+from ArticleSpider.items import ZhihuQuestionItem, ZhihuAnswerItem
+from datetime import datetime
 
 
 class ZhihuSpider(scrapy.Spider):
@@ -19,11 +23,29 @@ class ZhihuSpider(scrapy.Spider):
 
     User = namedtuple('User', ['account', 'passwd'])
     user = User('', '')
+    start_answer_url = "https://www.zhihu.com/api/v4/questions/{0}/answers?sort_by=default&include=data%5B%2A%5D." \
+                       "is_normal%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccollapsed_counts%2Crev" \
+                       "iewing_comments_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_" \
+                       "settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Crelationship.is_au" \
+                       "thor%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B%2A%5D.author.is_blockin" \
+                       "g%2Cis_blocked%2Cis_followed%2Cvoteup_count%2Cmessage_thread_token%2Cbadge%5B%3F%28type%3Dbe" \
+                       "st_answerer%29%5D.topics&limit={1}&offset={2}"
 
     def parse(self, response):
-        with open('1.html', 'wb') as f:
-            f.write(response.body)
+        with open('2.html', 'wb') as f:
+            f.write(response.text)
         pass
+
+    def parse_all_url(self, response):   # 获取当前页面所有的url
+
+        all_url = response.css('.question_link::attr(href)').extract()  # 提取当前界面所有问题url
+        for url in all_url:
+            match = re.match('.*?(/question/\d+).*?', url)  # 提取成该格式/question/54364750
+            if match:
+                url = match.group(1)
+            url = parse.urljoin(response.url, url)
+            print(url)
+            yield scrapy.Request(url, headers=self.headers, callback=self.parse_question)
 
     def start_requests(self):   # spider入口函数, 直接请求知乎登录界面
         return [scrapy.Request(url='https://www.zhihu.com/#signin',
@@ -66,16 +88,67 @@ class ZhihuSpider(scrapy.Spider):
             'captcha': captcha,
         }
         post_url = 'https://www.zhihu.com/login/phone_num'
-        yield scrapy.FormRequest(url=post_url, formdata=post_data,
-                                 headers=self.headers, callback=self.check_login)
+        return [scrapy.FormRequest(url=post_url, formdata=post_data,
+                                 headers=self.headers, callback=self.check_login)]
 
     def check_login(self, response):    # 通过服务器返回的数据判断是否登录成功
         text_json = json.loads(response.text)
         if 'msg' in text_json and text_json['msg'] == '登录成功':
             for url in self.start_urls:
-                yield scrapy.Request(url, dont_filter=True, headers=self.headers)
+                yield scrapy.Request(url, dont_filter=True, headers=self.headers, callback=self.parse_all_url)
+                # dont_filter表示该请求不应被调度程序过滤.当您要多次执行相同的请求时使用此选项,以忽略重复的过滤器, 请谨慎使用,否则您将进入爬行循环
+        else:
+            raise RuntimeError('登录失败!')
+
+    def parse_question(self, response):
+        question_id = 0
+        match = re.match(r'.*?question/(\d+).*?', response.url)
+        if match:
+            question_id = int(match.group(1))
+        loader = ItemLoader(ZhihuQuestionItem(), response=response)
+        loader.add_value('zhihu_id', question_id)   # 问题id
+        loader.add_css('topics', '.QuestionHeader-topics .Popover div::text')  # 话题
+        loader.add_value('url', response.url)  # 问题url
+        loader.add_css('title', '.QuestionHeader-content .QuestionHeader-main .QuestionHeader-title::text')  # 问题标题
+        loader.add_css('content', '.QuestionRichText .RichText::text')  # 问题的内容. 有可能为空
+        loader.add_css('answer_num', '.List-headerText span::text')  # 问题回答数量
+        loader.add_css('comments_num', '.QuestionHeader-actions button::text')  # 问题评论数量
+        loader.add_css('watch_user_num', 'button .NumberBoard-value::text')  # 关注问题的用户
+        loader.add_css('click_num', 'div.NumberBoard-item .NumberBoard-value::text')  # 问题浏览数量
+        loader.add_value('crawl_time', datetime.now())  # 问题爬取时间
+        item = loader.load_item()
+        yield item
+        yield scrapy.Request(self.start_answer_url.format(question_id, 20, 0), headers=self.headers,
+                             callback=self.parse_answer)
+
+    def parse_answer(self, response):
+        answers = json.loads(response.text)
+        is_end = answers['paging']['is_end']
+        next_url = answers['paging']['next']
+
+        for answer in answers['data']:
+            answer_item = ZhihuAnswerItem()
+            answer_item['zhihu_id'] = answer['id']  # 感觉是回答的id
+            answer_item['url'] = answer['url']
+            answer_item['question_id'] = answer['question']['id']
+            answer_item['author_id'] = answer["author"]["id"] if "id" in answer["author"] else None
+            answer_item['content'] = answer["content"] if "content" in answer else None  # 可能为空
+            answer_item['praise_num'] = answer['voteup_count']
+            answer_item['comments_num'] = answer['comment_count']
+            answer_item['create_time'] = answer['created_time']
+            answer_item['update_time'] = answer['updated_time']
+            answer_item['crawl_time'] = datetime.now()
+            yield answer_item
+        if not is_end:
+            scrapy.Request(next_url, headers=self.headers, callback=self.parse_answer)
 
 
-    def get_account(self):
-        self.user.account = '13281208165'
-        self.user.passwd = '123456..'
+
+
+'''
+1. 在知乎爬虫的入口函数请求知乎登录界面, 在完成请求响应后回调gen_captcha方法
+2. 在gen_captcha方法中获取_xsrf字段, 这个是知乎发送给浏览器的, 需要同用户密码一同提交
+3. 处理验证码相关逻辑, 并把得到_xsrf字段通过meta传递给login函数
+4. 将得到的数据交给FormRequest请求, 并回调检查登录函数.check_login, 最后回调parse函数
+
+'''
